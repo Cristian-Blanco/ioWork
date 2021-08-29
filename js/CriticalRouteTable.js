c307c8066edf344c33c558335e7ff74a05d7eb4e
Cristian Martinez <cristianjmb10@gmail.com>
2021-08-28 19:26:16 -0500

    Critical Route Table done

--- js/CriticalRouteTable.js
+++ js/CriticalRouteTable.js
@@ -1,16 +1,18 @@
+var listRouteCritical = [];
+
 const getData = () => {
     const numberDataEntry = document.getElementsByClassName('data-entry').length;
     let table = [];
     for (let index = 0; index < numberDataEntry; index++) {
         let activity = {
             'activity': document.getElementById(`activity-${index+1}`).value,
-            'dependence': document.getElementById(`dependence-${index+1}`).value,
+            'dependence': document.getElementById(`dependence-${index+1}`).value.split(","),
             'time': parseInt(document.getElementById(`time-${index+1}`).value,10),
             'tic': 0,
             'tfc': 0,
             'til': 0,
             'tfl': 0,
-            'holgura': 0
+            'slack': 0
         }
         table.push(activity);
     }
@@ -22,24 +24,79 @@ const minActivitiesTIL = (tableSelect) => Math.min.apply(Math, tableSelect.map(f
 
 const nearTime = (table) => {
     return table.map(function (element) {
-        if(element.dependence == '-') element.tic = 0;
+        if(element.dependence[0] == '-') element.tic = 0;
         else{
-            let lettersDependences = element.dependence.split(",");
-            let dependences = []
+            let dependences = [];
             //look for objects with dependencies
-            lettersDependences.forEach( letter => {
-                dependences.push(table.find( row => row.activity == letter))
+            element.dependence.forEach( letter => {
+                dependences.push(table.find( row => row.activity == letter));
             });
-            element.tic = maxActivitiesTFC(dependences)
+            element.tic = maxActivitiesTFC(dependences);
         }
         element.tfc = element.time + element.tic;
         return element;
     })
 }
 
+const farTime = (table) => {
+    const fartime = table.reverse().map(function (element) {
+        //we look for dependencies that have the activity
+        let activities = table.filter(activity => activity.dependence.includes(element.activity))
+        
+        if(activities.length == 0) element.tfl = maxActivitiesTFC(table);
+        else element.tfl = minActivitiesTIL(activities);
+        element.til = element.tfl - element.time;
+        return element;
+    })
+    return fartime.reverse()
+}
+
+const slack = (table) => {
+    return table.map(function (element) {
+        element.slack = element.tfl - element.tfc;
+        return element;
+    })
+}
+
+const routeCriticals = (letter, route, tableSlack, actualList = []) => {
+    const exist = tableSlack.find(row => row.activity == letter);
+    if(!exist) return false;
+    if(listRouteCritical.length == 0) listRouteCritical.push([letter])
+    else {
+        if(listRouteCritical.length < route){//create new route
+            actualList = [...actualList, letter]
+            listRouteCritical = [...listRouteCritical, actualList]
+        }else{
+            listRouteCritical[route-1] = [...listRouteCritical[route-1], letter]
+        }
+    }     
+    //we take the current list in case there is another critical path
+    actualList = listRouteCritical[route - 1];
+    exist.dependence.forEach(newLetter => {
+        route = routeCriticals(newLetter, route, tableSlack, actualList) ? route + 1 : route;
+    });
+    return true;
+}
+
 const processData = () => {
-    console.log('esto es getData: ',getData())
-    console.log('esto es nearTime: ', nearTime(getData()))
+    let process = getData();
+    process = nearTime(process);
+    process = farTime(process);
+    process = slack(process);
+    const tableSlack = process.filter(row => row.slack == 0);
+    let lastLetters = [];
+    tableSlack.forEach(element =>{
+        let hola = tableSlack.find( row => row.dependence.includes(element.activity))
+        !hola && lastLetters.push(element.activity) 
+    })
+    lastLetters.forEach(letter => {
+        routeCriticals(letter, listRouteCritical.length + 1, tableSlack);
+    })
+    return {
+        'process': process,
+        'lastLetters': lastLetters,
+        'listRouteCritical': listRouteCritical
+    }
 }
 
 
